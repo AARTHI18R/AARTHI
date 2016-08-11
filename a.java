40ed8eebbe67cc10d28a500bbd66c7df4b8ba3dd
AARTHI18R <aaarthi196@gmail.com>
2016-08-11 10:31:50 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,1 +1,16 @@
-
+import java.io.*;
+import java.util.*;
+class a
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    if(a>0)
+    System.out.println("positive");
+    else if(a==0)
+    System.out.println("zero")
+    else
+    System.out.println("negative");
+  }
+}
